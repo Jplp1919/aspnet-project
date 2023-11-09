a77cea10350b0cb8f49c37d20234553a3a44eb52
unknown <CRIATEC@ijagit05.unijui.intranet>
2023-11-09 09:01:14 -0300

    feat: Interfaces e Entities na camada Domain

---
+++ App.Domain/Interfaces/Application/IPessoaService.cs
@@ -0,0 +1,24 @@
+﻿using App.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Interfaces.Application
+{
+    internal interface IPessoaService
+    {
+        void Editar(Pessoa obj);
+
+        void Deletar(int id);
+
+        void criar(Pessoa obj);
+
+        Pessoa BuscarPorId(int id);
+
+        List<Pessoa> BuscarLista();
+
+
+    }
+}
---
+++ App.Domain/Interfaces/Repositories/IRepositoryBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Interfaces.Repositories
+{
+    public interface IRepositoryBase where TEntity : class
+    {
+        IOrderedQueryable<TEntity> Query(Expression<TEntity, bool>> where);
+        void Save(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(int id);
+
+        int saveChanges();
+
+        DbContext Context();
+
+
+
+
+
+    }
+}
