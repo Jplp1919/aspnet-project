811a6981203813b6d032c4886dae86cef9f9464f
Jplp1919 <jplpetter@gmail.com>
2023-11-16 15:36:39 -0300

    desenvolvimento do front

---
+++ App.Web/wwwroot/js/pages/pessoas-formulario.js
@@ -0,0 +1,14 @@
+﻿function salvar() {
+    let obj = {
+        nome: ($("[name='nome']").val() || ""),
+        nome: ($("[name='email']").val() || ""),
+        nome: ($("[name='senha']").val() || "")
+    };
+    PessoaCriar(obj).then(function () {
+        window.location.href = '/pessoas';
+    }, function (err) {
+        alert(err);
+    });
+
+
+}
---
+++ App.Web/wwwroot/js/pages/pessoas.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    load();
+});
+
+
+function load(){
+    PessoaListaPessoas().then(function (data) {
+        data.forEach(obj => {
+            $('#table tbody').append('' +
+                '<tr id="obj-' + obj.id + '">' +
+                '<td>' + (obj.Nome || '--') + '</td>' +
+                '<td>' + (obj.Email || '--') + '</td>' +
+                '<td>' + (obj.Id || '--') + '</td>' +
+                '</tr>'
+    );
+        });
+    });
+}
+
