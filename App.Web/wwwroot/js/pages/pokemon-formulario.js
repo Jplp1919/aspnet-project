ab563965d16e246c1cfd4783664a83b355ede1fc
Jplp1919 <jplpetter@gmail.com>
2023-11-20 08:12:45 -0300

    adicionar pokemon em progresso

--- App.Web/wwwroot/js/pages/pokemon-formulario.js
+++ App.Web/wwwroot/js/pages/pokemon-formulario.js
@@ -1,10 +1,10 @@
 ﻿function salvar() {
     let obj = {
-        Nome: ($("[name='Nome']").val() || ""),
-        Email: ($("[name='Level']").val() || ""),
-        Senha: ($("[name='Tipo']").val() || "")
+        Nome: ($("[name='Nome']").val() ),
+        Email: ($("[name='Level']").val() ),
+        Senha: ($("[name='Tipo']").val())
     };
-    PessoaCriar(obj).then(function () {
+    PokemonCriar(obj).then(function () {
         window.location.href = '/pokemon';
     }, function (err) {
         alert(err);
