fbb773f8b2746e3840df3c8b1238b25b8a742b37
Jplp1919 <jplpetter@gmail.com>
2023-11-20 15:18:12 -0300

    deletar adicionado em cidades

---
+++ App.Web/wwwroot/js/pages/cidades-delete.js
@@ -0,0 +1,11 @@
+﻿function deletar() {
+    let id = $('[name="Id"]').val();
+
+    CidadeDeletar(id).then(function () {
+        window.location.href = '/cidades';
+    }, function (err) {
+        alert(err);
+    });
+
+
+}
